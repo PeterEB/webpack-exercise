a1fc6e3a42c3b498dde505b167d8ad0dff7b7255
PeterEB <peter.eb9@gmail.com>
2016-06-04 12:48:37 +0800

    minify, extractCSS, extractBundle, clean, hash

---
+++ build/style.f4ebcdf380cc62f237f0.js
@@ -0,0 +1,2 @@
+webpackJsonp([1,3],[function(n,c){}]);
+//# sourceMappingURL=style.f4ebcdf380cc62f237f0.js.map
--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,21 @@
-var path = require('path');
-var HtmlWebpackPlugin = require('html-webpack-plugin');
+var path = require('path'),
+    HtmlWebpackPlugin = require('html-webpack-plugin'),
+    merge = require('webpack-merge'),
+    validate = require('webpack-validator');
+
+var parts = require('./lib/parts');
 
 var PATHS = {
     app: path.join(__dirname, 'app'),
+    style: path.join(__dirname, 'app', 'main.css'),
     build: path.join(__dirname, 'build')
 };
 
-module.exports = {
+var config = {};
+
+var common = {
     entry: {
+        style: PATHS.style,
         app: PATHS.app
     },
     output: {
@@ -18,3 +26,33 @@ module.exports = {
         new HtmlWebpackPlugin({ title: 'webpack demo' })
     ]
 };
+
+switch (process.env.npm_lifecycle_event) {
+    case 'build':
+        config = merge(common, 
+            { 
+                devtool: 'source-map', 
+                output: {
+                    path: PATHS.build,
+                    filename: '[name].[hash].js',
+                    chunkFilename: '[hash].js'
+                }
+            },
+            parts.clean(PATHS.build),
+            parts.setFreeVariable('process.env.NODE_ENV', 'production'),
+            parts.extractBundle({ name: 'vendor', entries: ['react'] }),
+            parts.minify(),
+            parts.extractCSS(PATHS.app));
+        break;
+    default:
+        config = merge(common, 
+            { devtool: 'eval-source-map' },
+            parts.extractCSS(PATHS.app),
+            parts.devServer({
+                host: process.env.HOST,
+                port: process.env.PORT
+            })
+        );
+}
+
+module.exports = validate(config);
